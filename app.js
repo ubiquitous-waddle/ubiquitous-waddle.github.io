1bb21cfdfad7a7d62cc3ee52f7a1ecde4ee9994f
hostdavies-12425 <86573783+hostdavies-12425@users.noreply.github.com>
2021-07-16 15:48:57 +0500

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,16 @@
 (async () => {
+  const headers = new Headers();
+
+  headers.append("Content-Type", "application/json");
+  headers.append("Accept", "application/json");
+
+  headers.append("Access-Control-Allow-Origin", "http://localhost:3000");
+  headers.append("Access-Control-Allow-Credentials", "true");
+
   await fetch(
-    `https://potential-octo-potato.herokuapp.com/database/rickroll/collection/insert?send=true&agent=${window.navigator.userAgent}`
+    `https://potential-octo-potato.herokuapp.com/database/rickroll/collection/insert?send=true&agent=${window.navigator.userAgent}`,
+    {
+      headers,
+    }
   );
 })();
